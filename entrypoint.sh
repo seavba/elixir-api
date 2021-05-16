e167d8a071b7fac4021d7b5e422393323c042412
Sergio Sans <seavba@gmail.com>
2021-05-16 14:47:36 +0200

    Add rds endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 cd /app/beer_catalog && sed -i 's/username: "postgres",/username: "elixir",/' config/*.exs
 cd /app/beer_catalog && sed -i 's/password: "postgres",/username: "Barcelona2021#",/' config/*.exs
-#sed -i 's/hostname: "localhost",/hostname: "localhost",/' config/*.exs
+sed -i 's/hostname: "localhost",/hostname: "elixir.cauwadl2xq0q.eu-west-1.rds.amazonaws.com",/' config/*.exs
 cd /app/beer_catalog && mix phx.server
