c38cbfa838440fdef97b90d0540639abedbcd0d6
Sergio Sans <seavba@gmail.com>
2021-05-16 16:08:12 +0200

    Adding all commands needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,10 @@ set -euo pipefail
 cd /app/beer_catalog && sed -i 's/username: "postgres",/username: "elixir",/' config/*.exs
 cd /app/beer_catalog && sed -i 's/password: "postgres",/username: "Barcelona2021#",/' config/*.exs
 cd /app/beer_catalog && sed -i 's/hostname: "localhost",/hostname: "elixir.cauwadl2xq0q.eu-west-1.rds.amazonaws.com",/' config/*.exs
+
+cd /app/beer_catalog && mix ecto.create
+cd /app/beer_catalog && mix phx.gen.context Catalog Beer beers brand:string style:string origin:string quantity:integer
+cd /app/beer_catalog && mix ecto.migrate
+cd /app/beer_catalog && mix run priv/repo/seeds.exs
+cd /app/beer_catalog && echo Y | mix phx.gen.json Catalog Beer beers brand:string style:string origin:string quantity:integer --no-context --no-schema
 cd /app/beer_catalog && mix phx.server
